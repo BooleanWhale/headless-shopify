02d49d2c5e2c678fd5074b5841efbf5c6c4b43a3
Ashley Saleem-west <BooleanWhale@users.noreply.github.com>
2021-10-07 13:42:47 +0100

    animations and ico

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,13 @@ import { Box, Button, Text, Image, Center } from "@chakra-ui/react"
 const Hero = () => {
   return (
     <Box background="#FFA8E2" w="100%" position="relative" h={["60vh","50vh"]} pb="2rem">
-      <Image src="https://iconape.com/wp-content/files/zk/93042/svg/react.svg" 
+      <Image src="https://iconape.com/wp-content/files/zk/93042/svg/react.svg"  className="scale-in-center"
         h="100%" margin="auto" p="1rem 1rem 10rem" objectFit="contain" objectPosition="top"/>
-      <Text position="absolute" bottom="15%" width="100%" textAlign="center" color="white" fontWeight="bold" fontSize="4rem">
+      <Text 
+      position="absolute"
+      bottom="15%" width="100%" 
+      textAlign="center" color="white" 
+      fontWeight="bold" fontSize="4rem">
         Some hero section text
       </Text>
       <Center>
